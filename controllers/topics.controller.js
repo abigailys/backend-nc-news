7a135545c100f76c98f66876b732b41edd5245ac
Abigail Lee <226848368+abigailys@users.noreply.github.com>
2026-02-05 15:41:29 +0000

    update formatting

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -2,5 +2,5 @@ const { retrieveTopics } = require("../services/topics.service.js")
 
 exports.getTopics = async (request, response) => {
     const topics = await retrieveTopics()
-    response.status(200).send({topics: topics});
+    response.status(200).send({ topics: topics });
 }
