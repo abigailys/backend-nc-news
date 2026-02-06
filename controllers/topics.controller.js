5dca8d0ce1e92eb9ea6322db150ba62fe6ee7f9c
Abigail Lee <226848368+abigailys@users.noreply.github.com>
2026-02-06 13:45:22 +0000

    add try/catch blocks to catch errors

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,6 +1,11 @@
 const { retrieveTopics } = require("../services/topics.service.js")
 
-exports.getTopics = async (request, response) => {
-    const topics = await retrieveTopics()
-    response.status(200).send({ topics: topics });
+exports.getTopics = async (request, response, next) => {
+    try {
+        const topics = await retrieveTopics()
+        response.status(200).send({ topics: topics });
+    }
+    catch (error) {
+        next(error);
+    };
 }
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,6 +1,11 @@
 const { retrieveUsers } = require("../services/users.service.js")
 
-exports.getUsers = async (request, response) => {
-    const users = await retrieveUsers();
-    response.status(200).send({ users: users });
+exports.getUsers = async (request, response, next) => {
+    try {
+        const users = await retrieveUsers();
+        response.status(200).send({ users: users });
+    }
+    catch (error) {
+        next(error);
+    };
 }
